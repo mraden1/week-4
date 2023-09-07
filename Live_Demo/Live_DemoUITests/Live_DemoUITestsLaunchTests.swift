1d5def3ee16ef6910120a9aaff4da7378800ac0d
Mark Raden <mraden@csu.fullerton.edu>
2023-09-07 16:59:21 -0700

    I switched my branch from main to master and have started an xcode project

---
+++ Live_Demo/Live_DemoUITests/Live_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Live_DemoUITestsLaunchTests.swift
+//  Live_DemoUITests
+//
+//  Created by Mark D. Raden on 9/7/23.
+//
+
+import XCTest
+
+class Live_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
